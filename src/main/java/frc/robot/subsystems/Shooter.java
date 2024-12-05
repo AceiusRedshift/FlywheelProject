eb4e2a32517f886cd9fb85360eb34d30bf72e539
Aceius E. <aceiuselms@seattleacademy.org>
2024-12-04 17:54:49 -0800

    Finish shooter system

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,9 +1,24 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.playingwithfusion.TimeOfFlight;
+import com.playingwithfusion.TimeOfFlight.RangingMode;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.math.filter.MedianFilter;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 // Throngler
 public class Shooter extends SubsystemBase {
+    private final WPI_VictorSPX flywheel1;
+    private final WPI_VictorSPX flywheel2;
+    private final TimeOfFlight lidar;
+    private final CANSparkMax intake;
+
+    private final MedianFilter sensorMedianFilter = new MedianFilter(5);
+
     /**
      * Make sure these IDs are correct - activating the wrong motor could cause harm
      * to life or property.
@@ -18,7 +33,13 @@ public class Shooter extends SubsystemBase {
      * @param lidarId          The CAN ID of the lidar time-of-flight sensor.
      */
     public Shooter(int firstFlywheelId, int secondFlywheelId, int intakeId, int lidarId) {
-        //
+        flywheel1 = new WPI_VictorSPX(firstFlywheelId);
+        flywheel2 = new WPI_VictorSPX(secondFlywheelId);
+
+        intake = new CANSparkMax(intakeId, MotorType.kBrushless);
+
+        lidar = new TimeOfFlight(lidarId);
+        lidar.setRangingMode(RangingMode.Short, 25);
     }
 
     /**
@@ -27,7 +48,9 @@ public class Shooter extends SubsystemBase {
      * @param speed -1.0 to 1.0.
      */
     public void setFlywheelSpeed(double speed) {
-        //
+        // Why is speed negative?? motors on backward or smthing idk
+        flywheel1.set(-speed);
+        flywheel2.set(-speed);
     }
 
     /**
@@ -36,27 +59,38 @@ public class Shooter extends SubsystemBase {
      * @param speed -1.0 to 1.0.
      */
     public void setGrabberSpeed(double speed) {
-        //
+        intake.set(speed);
     }
 
     /**
      * @return If there is a note in the intake.
      */
     public boolean hasNoteInIntake() {
-        return false;
+        return getLidarDistance() < Units.inchesToMeters(3);
     }
 
     /**
      * Eject the contents of the shooter.
      */
     public void eject() {
-        //
+        setGrabberSpeed(-1.0);
+        setGrabberSpeed(-0.1);
     }
 
     /**
      * IMMEDIATELY stop all motors.
      */
     public void stop() {
-        //
+        setFlywheelSpeed(0);
+        setGrabberSpeed(0);
+
+        // Brake motors
+        flywheel1.stopMotor();
+        flywheel2.stopMotor();
+        intake.stopMotor();
+    }
+
+    private double getLidarDistance() {
+        return sensorMedianFilter.calculate(lidar.getRange() / 1000);
     }
 }
